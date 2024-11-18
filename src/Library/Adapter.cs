a694b34e2b4ec10740deeeb3434cacc694ff94dc
cogliattifede <federico.cogliatti@correo.ucu.edu.uy>
2024-11-18 00:29:25 -0300

    haciendo el ejercicio

---
+++ src/Library/Adapter.cs
@@ -0,0 +1,16 @@
+namespace Full_GRASP_And_SOLID;
+
+public class Adapter : TimerClient
+{
+    private Recipe recipe;
+    
+    public Adapter(Recipe recipe)
+    {
+        this.recipe = recipe;
+    }
+
+    public void TimeOut()
+    {
+        this.recipe.MarkAsCooked();
+    }
+}
--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -6,6 +6,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Full_GRASP_And_SOLID
 {
@@ -15,6 +16,8 @@ namespace Full_GRASP_And_SOLID
         private IList<BaseStep> steps = new List<BaseStep>();
 
         public Product FinalProduct { get; set; }
+        
+        public bool Cooked { get; private set; } = false;
 
         // Agregado por Creator
         public void AddStep(Product input, double quantity, Equipment equipment, int time)
@@ -62,5 +65,54 @@ namespace Full_GRASP_And_SOLID
 
             return result;
         }
+
+        public int GetCoockTime()
+        {
+            int result = 0;
+
+            foreach (BaseStep step in this.steps)
+            {
+                result += step.Time;
+            }
+
+            return result;
+        }
+
+        public void Cook()
+        {
+            if (this.Cooked)
+            {
+                return;
+            }
+            
+            int cookTime = GetCoockTime();
+
+            CountdownTimer timer = new CountdownTimer();
+
+            Adapter adapter = new Adapter(this);
+            
+            timer.Register(cookTime, client: adapter );
+        }
+        
+        public void MarkAsCooked()
+        {
+            this.Cooked = true;
+        }
     }
 }
+
+/*
+Las guías que fueron utilizadas: 
+
+El de Expert, ya qué en GetCookTime() la responsabilidad de calcular el tiempo de cocción es asignada
+a la clase que tiene la información de calcular el tiempo de cocción.
+
+El de Creator, pues cree una instancia de CountdownTimer dentro de Cook() , quien tiene conocimiento de 
+la clase Adapter.
+
+Bajo Acoplamiento, el Adapter permite que la clase Cook no dependa directamente de la implementación del temporizador.
+
+El de DIP, ya qué cree el método MarkAsCooked() en la clase Recipe, 
+logrando que la clase Recipe no dependa de la clase Adapter. Y tenga una única responsabilidad.
+Además, la responsabilidad de calcular el tiempo está separada en el método GetCookTime(). 
+*/
